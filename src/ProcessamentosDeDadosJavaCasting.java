041b931ddb3638bd15579331a25e83e5275663f3
Tiago Ribeiro <tgwebmaster27@gmail.com>
2022-11-24 13:10:56 -0300

    Processamento de dados em Java, Casting

---
+++ src/ProcessamentosDeDadosJavaCasting.java
@@ -0,0 +1,49 @@
+
+
+public class ProcessamentosDeDadosJavaCasting {
+
+	public static void main(String[] args) {
+		/**
+		int x, y;
+		
+		x=5;
+		y=2*x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		*/
+		
+		/**
+		int x;
+		double y;
+		
+		x=5;
+		y=2*x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		*/
+		
+		/*
+		 * Casting
+		 *É a conversão explícita de um tipo para outro.
+		 *É necessário quando o compilador não é capaz de “adivinhar” que o
+		 *resultado de uma expressão deve ser de outro tipo.
+		 * 
+		 * */
+		
+		//Exemplo de Casting
+		
+		int a, b;
+		double resultado;
+		a = 5;
+		b = 2;
+		resultado = (double) a / b;
+		System.out.println(resultado);
+		
+		
+		
+
+	}
+
+}
