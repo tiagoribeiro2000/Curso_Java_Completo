17c2105f66a6bada37a6f4bdc7b0ee4c779057e1
Tiago Ribeiro <tgwebmaster27@gmail.com>
2022-11-24 10:55:53 -0300

    Iniciando curso programacao Java Completo

---
+++ src/SaidaDeDados.java
@@ -0,0 +1,28 @@
+
+public class SaidaDeDados {
+
+	public static void main(String[] args) {
+		
+		int y = 32;
+		double x = 10.35784;
+		
+		
+	String product1 = "Computer";
+	String product2 = "Office desk";
+	
+	int age = 30;
+	int code=5290;
+	char gender = 'F';
+	
+	double price1 = 2100.0;
+	double price2=650.50;
+	double measure = 53.234567;
+	
+	System.out.println("Products:");
+	System.out.println(product1+","+" which price is "+price1);
+	System.out.println(product2+","+"which price is "+price2);
+    System.out.println("Record "+age+" years old, code "+code+" and gender: "+gender);
+
+	}
+
+}
