86b3ab1815c893fe6305edee8859d12ac4372dad
Tiago Ribeiro <47039818+tiagoribeiro2000@users.noreply.github.com>
2023-03-11 13:49:15 -0300

    Add files via upload
    Exercício de programação orientada a objetos Calculo do triangulo.

---
+++ src/Calculo_Do_Triangulo/src/application/Triangle.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Triangle {
+
+	public double a;
+	public double b;
+	public double c;
+	
+	
+	public Double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		
+	}
+}
