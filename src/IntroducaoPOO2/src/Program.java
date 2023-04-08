83631700573ea92cb326bcba817a8abfb53490ac
Tiago Ribeiro <47039818+tiagoribeiro2000@users.noreply.github.com>
2023-04-08 15:36:41 -0300

    Add files via upload
    Exercicio de programação orientada a objetos java parte 2.

---
+++ src/IntroducaoPOO2/src/Program.java
@@ -0,0 +1,49 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Products;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Products products  = new Products();
+		
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		products.name = sc.nextLine();
+		System.out.print("Price: ");
+		products.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		products.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data: "+products);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		products.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Product data: "+products);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		
+		quantity = sc.nextInt();
+		products.removeProducts(quantity);
+		
+
+		System.out.println();
+		System.out.println("Product data: "+products);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
